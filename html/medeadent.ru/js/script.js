afaf60bb797804630228838b6e2e8f2da7761a04
elminate <elminate@icloud.com>
2021-12-05 19:46:31 +0500

    в

--- html/medeadent.ru/js/script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector('.header__hamburger'),
-    menu = document.querySelector('.header__nav');
-
-hamburger.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    hamburger.classList.toggle('active');
-});
